64ac632bd451d86a8495cec8562861b1218fd84c
shamil abubacker <abzshamil@gmil.com>
2021-05-02 11:52:17 -0400

    portfolio

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,4 @@
-import react, {useEffect,useState} from "react";
+import React, {useEffect,useState} from "react";
 import sanityClient from "../client.js";
 import trees from "../trees.jpg";
 import imageUrlBuilder from "@sanity/image-url"
